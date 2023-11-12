96fc3c59679a248947584ded978359c01a890ecc
jarin <jarin.nemec@gmail.com>
2023-11-12 23:04:03 +0100

    bum up version

--- update/main.py
+++ update/main.py
@@ -4,14 +4,15 @@ from update.install_galg.galg import create_galg_package_for_all_companies
 output_dir = "c:/galg/manager-root/bizerba/data_dev/filetrack/update"
 tmp_dir = "c:/galg/manager-root/bizerba/data_dev/filetrack/tmp"
 
+
 def docker():
-    version = "1.1.10"
+    version = "1.2.0"
     create_docker_package_for_all_companies(version, tmp_dir, output_dir)
 
 
 def galg():
-     version = "1.1.10"
-     create_galg_package_for_all_companies(version, tmp_dir, output_dir)
+    version = "1.2.0"
+    create_galg_package_for_all_companies(version, tmp_dir, output_dir)
 
 
 if __name__ == "__main__":
